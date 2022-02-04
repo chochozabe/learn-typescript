8951a95c55be7e2ce63371625361f7c439c920c8
chochozabe <cintamani_whyk@naver.com>
2022-02-04 10:49:45 +0900

    모듈화 실습

--- quiz/2_address-book/src/index.ts
+++ quiz/2_address-book/src/index.ts
@@ -1,23 +1,8 @@
-interface PhoneNumberDictionary {
-  [phone: string]: {
-    num: number;
-  };
-}
-
-interface Contact {
-  name: string;
-  address: string;
-  phones: PhoneNumberDictionary;
-}
-
-enum PhoneType {
-  Home = 'home',
-  Office = 'office',
-  Studio = 'studio'
-}
-
 // api
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
+
+import { Contact, PhoneType } from "./types";
+
 // api 호출 시 제너릭 가장 많이 사용
 function fetchContacts() : Promise<Contact[]> {
   // TODO: 아래 변수의 타입을 지정해보세요.
